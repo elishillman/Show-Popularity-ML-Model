5b2e10691cd32d8892f3edf2a73bc20005833831
Eli Shillman <elishillman@Elis-MBP.fios-router.home>
2023-09-24 18:57:42 -0400

    Completed Web App Functionality

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,8 +1,22 @@
-#from .model import predictShow
-from flask import Flask, render_template
+from .model import predictShow
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('input_form.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    rating = float(request.form['imdb_rating'])
+    votes = int(request.form['imdb_votes'])
+    service = request.form['streaming_service']
+    comments = int(request.form['reddit_comments'])
+    followers = int(request.form['twitter_followers'])
+    status = request.form['status']
+    runtime = int(request.form['runtime'])
+    genre = request.form['genre']
+    prediction = predictShow(rating, votes, service, comments, followers, status, runtime, genre)
+    
+    return render_template('result.html', prediction=prediction)
